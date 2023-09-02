761607797d1c2f4816cb2f04829259736db18a2d
gurjnderSingh <gurjindersingh663@gmail.com>
2023-09-02 23:26:05 +0530

    initial commit

---
+++ ComibineExample/ComibineExample/ComibineExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComibineExampleApp.swift
+//  ComibineExample
+//
+//  Created by Gurjinder Singh on 02/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ComibineExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
